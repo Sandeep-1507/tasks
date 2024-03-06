686e6c34ac428ae2cf6f10012c60837328170bd9
Sandeep Patel <96694484+Sandeep-1507@users.noreply.github.com>
2024-03-06 15:30:33 +0530

    Added support for i3xrocks

---
+++ i3xrocks/default.nix
@@ -0,0 +1,44 @@
+let
+
+  nixpkgs = fetchTarball "https://github.com/NixOS/nixpkgs/tarball/nixos-23.11";
+
+  pkgs = import nixpkgs { config = {}; overlays = []; };
+
+in
+
+
+pkgs.stdenv.mkDerivation {
+pname= "i3xrocks";
+version="v1.0";
+
+src= pkgs.fetchFromGitHub{
+owner= "regolith-linux";
+repo="i3xrocks";
+rev="master";
+sha256="a8RwSoZElGwqkxnRtn/FeNMzXeS8PnfXhMQpYSuYD0I=";
+};
+
+buildInputs=with pkgs; [
+    autoconf
+    automake   
+    gcc
+    gdb
+nix-bash-completions
+pkg-config
+gnumake
+xcbutilxrm
+xorg.xcbutil
+  ];
+
+buildPhase = ''
+    ./autogen.sh
+    ./configure
+    make 
+  '';
+
+  installPhase = ''
+    make install DESTDIR=$out
+  '';
+  
+
+}
