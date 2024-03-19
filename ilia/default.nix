36ba7a1038af22f79841ade1d64bad177c03d61c
Sandeep Patel <96694484+sandptel@users.noreply.github.com>
2024-03-19 05:49:56 +0530

    Update default.nix

--- ilia/default.nix
+++ ilia/default.nix
@@ -1,28 +1,5 @@
 { pkgs ? import (fetchTarball "https://github.com/NixOS/nixpkgs/archive/06278c77b5d162e62df170fec307e83f1812d94b.tar.gz") {}}:
 
-let
-
-  oldnixpkgsCommit = "dd1b7e377f6d77ddee4ab84be11173d3566d6a18";
-  newnixpkgsCommit = "18a0b68e42bebce4b75402f586a0cb43da626ce6";
-
-
-  nixpkgsSrc1 = builtins.fetchTarball {
-    url = "https://github.com/NixOS/nixpkgs/archive/${oldnixpkgsCommit}.tar.gz";
-    sha256 = "158zx28wfxqkv51lwgkbsdp1v3wyijp6crmk3mkvqa0v0619yxiv"; 
-  };
-
-
-  oldpkgs = import nixpkgsSrc1 {};
-  nixpkgsSrc2 = builtins.fetchTarball {
-    url = "https://github.com/NixOS/nixpkgs/archive/${newnixpkgsCommit}.tar.gz";
-    sha256 = "158zx28wfxqkv51lwgkbsdp1v3wyijp6crmk3mkvqa0v0619yxiv"; 
-  };
-
-
-  pkgs = import (fetchTarball "https://github.com/NixOS/nixpkgs/archive/06278c77b5d162e62df170fec307e83f1812d94b.tar.gz") {};
-
-in
-
 pkgs.stdenv.mkDerivation {
   pname = "ilia";
   version = "v0.5";
@@ -31,12 +8,13 @@ pkgs.stdenv.mkDerivation {
     owner = "regolith-linux";
     repo = "ilia";
     rev = "ubuntu/jammy";
-    sha256 = "03nncqk8b075sxzjfa5azh6pxdqzirm8zj87zr6z6gxmbm6l25vi";
+    sha256 = "4MKVwaepLOaxHFSwiks5InDbKt+B/Q2c97mM5yHz4eU=";
   };
 
 buildInputs = [
     pkgs.ninja
     pkgs.meson
+    pkgs.makeWrapper
   ];
 
  propagatedBuildInputs = [
@@ -48,18 +26,17 @@ buildInputs = [
     pkgs.json-glib
     pkgs.cairo
     pkgs.pango
-    oldpkgs.tracker
+    pkgs.tracker
+    pkgs.gtk-layer-shell
     pkgs.gobject-introspection
   ];
-
-
-  buildPhase = ''
-    meson ..
-    ninja 
-  '';
-
+  
   installPhase = ''
     mkdir -p $out/bin
-    cp src/ilia $out/bin/
+    mkdir -p $out/share
+    cp src/ilia $out/share    
+    cp ../data/org.regolith-linux.ilia.gschema.xml $out/share
+    glib-compile-schemas $out/share/ 
+    makeWrapper $out/share/ilia $out/bin/ilia --set GSETTINGS_SCHEMA_DIR $out/share
   '';
 }
