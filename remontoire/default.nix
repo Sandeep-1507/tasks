a8e9904e285a8d2701beaff478710c0c3721a668
Sandeep Patel <96694484+sandptel@users.noreply.github.com>
2024-03-26 23:40:16 +0530

    Added Derivation for Remointiore

---
+++ remontoire/default.nix
@@ -0,0 +1,35 @@
+{ pkgs ? import (fetchTarball "https://github.com/NixOS/nixpkgs/archive/06278c77b5d162e62df170fec307e83f1812d94b.tar.gz") {}}:
+
+pkgs.stdenv.mkDerivation {
+  pname = "remontoire";
+  version = "v1.0";
+
+  src = pkgs.fetchFromGitHub {
+    owner = "regolith-linux";
+    repo = "remontoire";
+    rev = "master";
+    sha256 = "Cb6tzTGZdQA9oA04DO/xLBw5F+FRj5BM2Aa62YWGmZA=";
+  };
+
+buildInputs = [
+    pkgs.ninja
+    pkgs.meson
+    pkgs.makeWrapper
+  ];
+
+ propagatedBuildInputs = [
+    pkgs.glib
+    pkgs.vala
+    pkgs.pkg-config
+    pkgs.json-glib
+    pkgs.tracker
+    pkgs.gtk3
+    pkgs.libgee
+    pkgs.gobject-introspection
+  ];
+  
+  installPhase = ''
+    mkdir -p $out/bin
+    cp src/remontoire $out/bin   
+  '';
+}
